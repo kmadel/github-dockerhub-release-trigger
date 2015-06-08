21507d610c3e7419ad461d7bf6c89dda65537e0d
Kurt Madel <kmadel@mac.com>
2015-06-08 11:31:52 -0400

    Update flow.groovy

--- flow.groovy
+++ flow.groovy
@@ -11,12 +11,17 @@ echo "gitUrl: " + gitUrl
 node('docker') {
 	docker.withServer('tcp://127.0.0.1:1234'){
 	  docker.withRegistry('https://registry.hub.docker.com/', 'docker-registry-kmadel-login') {
+	  	stage 'SCM Checkout'
+	  	echo 'checkout from ${gitUrl} for refs/tags/${tagName}'
 	  	git url: '${gitUrl}'
 	  	checkout scm: [$class: 'GitSCM', 
 	  	  userRemoteConfigs: [[url: '${gitUrl}']], 
 	  	  branches: [[name: 'refs/tags/${tagName}']]], changelog: false, poll: false
+	  	stage 'Build Image'
+	  	echo 'building ${repoName}:${tagName}'
 	  	def image = docker.build "${repoName}:${tagName}"
-	  	stage 'Push Image ${repoName}:${tagName}'
+	  	stage 'Push Image'
+	  	echo 'pusing ${repoName}:${tagName}'
 	  	image.push '${tagName}'
 	}
 }
